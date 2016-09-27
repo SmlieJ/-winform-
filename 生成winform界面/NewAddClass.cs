8719ff1de784a6056a627e88e87e10fb3eaeaddc
SmlieJ <65716420@qq.com>
2016-09-27 12:06:18 +0800

    333

--- 生成winform界面/NewAddClass.cs
+++ 生成winform界面/NewAddClass.cs
@@ -16,17 +16,35 @@ namespace 生成winform界面
         Oracle.OracleSet OracleSet = new Oracle.OracleSet();
         #endregion
 
-        private void ReturnTable(string TableName)
+        /// <summary>
+        /// 返回数据库的表名
+        /// </summary>
+        /// <param name="TableName">数据库名</param>
+        /// <param name="DataBase">数据库类型</param>
+        /// <returns></returns>
+        private System.Data.DataTable ReturnTable(string TableName,string DataBase)
         {
             string sql = string.Empty;
+            System.Data.DataTable dt = new System.Data.DataTable();
+            switch (DataBase)
+            {
+                case "SQL Server":
+                    sql = string.Format("select name from {0}.sys.tables",TableName);
+                    dt = SQLSet.ExecutTable(sql);
+                    return dt;
+                case "Access": return dt; 
+                case "Oracle": return dt; 
+                case "ListSQL": return dt; 
+            }
+            return dt;
         }
 
-        private void GetColumnsType(string TableName)
+        private void GetColumnsType(string TableName, string DataBase)
         {
            
         }
 
-        private void ReturnTableName(string SoureName)
+        private void ReturnTableName(string SoureName, string DataBase)
         {
 
         }
